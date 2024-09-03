772518552c811c39279d843f2dafea45ff76d7f2
sickk <jaakmyr@gmail.com>
2024-09-03 11:09:56 -0700

    temp hardcoding url

--- src/components/OutputSection.jsx
+++ src/components/OutputSection.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useContext } from 'react'
 import io from 'socket.io-client'
 import { messageContext } from '../pages/mainPage.jsx'
-const socket = io.connect('http://localhost:8000')
+const socket = io.connect('https://react-pictochat-backend.onrender.com:8000')
 import '../css/outputSection.css'
 
 
