eb9fe92184967ea31e8664d2b74efe4f8eae77d0
sickk <jaakmyr@gmail.com>
2024-09-06 12:47:10 -0700

    added keyboard buttons

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,7 +1,26 @@
 
 function Keyboard() {
+    const keys = [
+        ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '='],
+        ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'DEL'],
+        ['CAPS', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ENTER'],
+        ['SHIFT', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'],
+        [';', '`', 'SPACE', '[', ']'],
+      ];
+
   return (
-    <div>Keyboard</div>
+    <div>
+        {keys.map((keyarray, i) => {
+            return(
+            <div key={i}>
+                {keyarray.map((key,i) => {
+                    return <button key={i}>{key}</button>
+                })}
+                
+            </div>
+            )
+        })}
+    </div>
   )
 }
 
