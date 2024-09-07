5a94b6cd3d7fa3dd381cd5e2e329c1cb384ddccd
sickk <jaakmyr@gmail.com>
2024-09-06 22:40:03 -0700

    added ui for the toolbox near keyboard. added images aswell

--- src/components/KeyboardToolBoxSidebar.jsx
+++ src/components/KeyboardToolBoxSidebar.jsx
@@ -5,9 +5,9 @@ function KeyboardToolBoxSidebar() {
     const {sendMessage, setImage, clear} = useContext(DrawingContext);
   return (
     <div className="BottomHalf--Toolbox">
-        <button onClick={() => sendMessage()} className="toolbar__button">send</button>
-        <button onClick={() => setImage()} className="toolbar__button">getLast</button>
-        <button onClick={() => clear()} className="toolbar__button">clear</button>
+        <button onClick={() => sendMessage()} className="toolbar__button"><img className="toolbar__img" src='assets-pictochat/upload.png' /></button>
+        <button onClick={() => setImage()} className="toolbar__button"><img className="toolbar__img" src='assets-pictochat/copy_image.png' /></button>
+        <button onClick={() => clear()} className="toolbar__button BottomHalf--Toolbox__Clear"><img className="toolbar__img " src='assets-pictochat/clean.png' /></button>
     </div>
     
   )
