e7e677da447e9ff7fb79330d17a5730a355581f5
sickk <jaakmyr@gmail.com>
2024-09-06 22:51:01 -0700

    added images for normal toolbox

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -9,10 +9,10 @@ function Toolbar() {
         
   return (
     <div id="toolbar">
-    <button onClick={() => setButtonWidth(2)} className={`${isButtonSelected(2, buttonWidth)} toolbar__button`}>sizeSmall</button>
-    <button onClick={() => setButtonWidth(5)} className={`${isButtonSelected(5, buttonWidth)} toolbar__button`}>sizeLarge</button>
-    <button onClick={() => setColor("black")} className={`${isButtonSelected("black", color)} toolbar__button`}>pencil</button>
-    <button onClick={() => setColor("white")} className={`${isButtonSelected("white", color)} toolbar__button`}>eraser</button>
+    <button onClick={() => setButtonWidth(2)} className={`${isButtonSelected(2, buttonWidth)} toolbar__button toolbar__button__circle`}><img className="toolbar__img " src='assets-pictochat/brush_medium.png' /></button>
+    <button onClick={() => setButtonWidth(5)} className={`${isButtonSelected(5, buttonWidth)} toolbar__button toolbar__button__circle`}><img className="toolbar__img " src='assets-pictochat/brush_large.png' /></button>
+    <button onClick={() => setColor("black")} className={`${isButtonSelected("black", color)} toolbar__button toolbar__button__circle`}><img className="toolbar__img " src='assets-pictochat/pencil.png' /></button>
+    <button onClick={() => setColor("white")} className={`${isButtonSelected("white", color)} toolbar__button toolbar__button__circle`}><img className="toolbar__img " src='assets-pictochat/eraser.png' /></button>
     </div>
   )
 }
